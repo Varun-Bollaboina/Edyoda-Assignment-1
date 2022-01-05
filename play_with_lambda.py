925be5d8d4c0bb5e1857fc6b0e9b101794590932
Varun-Bollaboina <94756305+Varun-Bollaboina@users.noreply.github.com>
2022-01-05 22:41:24 +0530

    Assignment-4 play with lambda

---
+++ play_with_lambda.py
@@ -0,0 +1,7 @@
+""" Lambda Function, also referred to as 'Anonymous function' is same as a regular python function but can be defined without a name. 
+While normal functions are defined using the def keyword, anonymous functions are defined using the 
+lambda keyword"""
+
+
+a = lambda b: b+25
+print(a(10))
