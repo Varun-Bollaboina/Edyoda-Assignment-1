304a74b91f88745b4b7c1d78ffed9d3d0afc1678
Varun-Bollaboina <94756305+Varun-Bollaboina@users.noreply.github.com>
2022-01-09 20:31:31 +0530

    Assignment - 5 Create You own built in class method

---
+++ Create_You_own_built_in_class_method.py
@@ -0,0 +1,31 @@
+#method : 1
+
+class Power:
+    def pow(self, x, n):
+        if x==-1:
+            if n%2 ==0:
+                return 1
+            else:
+                return -1
+        if x==0 or x==1 or n==1:
+            return x 
+        if n==0:
+            return 1
+        if n<0:
+            return 1/self.pow(x,-n)
+        val = self.pow(x,n//2)
+        if n%2 ==0:
+            return val*val
+        return val*val*x
+print(Power().pow(10,2))
+
+
+#method : 2
+
+class Power:
+    def pow(self,x,n):
+        a=1
+        for i in range(n):
+            a=x*a
+        print(a)
+Power().pow(10,2)
